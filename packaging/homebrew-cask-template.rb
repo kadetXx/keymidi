3d01be91475c92a073d1f68b21e92a99c351e0a1
Collins Enebeli <techkadet@gmail.com>
2026-06-12 22:52:15 +0200

    chore: add cask template for homebrew

--- packaging/homebrew-cask-template.rb
+++ packaging/homebrew-cask-template.rb
@@ -8,14 +8,14 @@
 #   shasum -a 256 release/KeyMIDI-<version>-x64.dmg
 
 cask "keymidi" do
-  version "0.1.0"
+  version "0.2.8"
 
   on_arm do
-    sha256 "REPLACE_WITH_ARM64_DMG_SHA256"
+    sha256 "b90b452da151f7c45bc450b687aab4e6abafb27917618a693a80c8b1761141f9"
     url "https://github.com/kadetXx/keymidi/releases/download/v#{version}/KeyMIDI-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "REPLACE_WITH_X64_DMG_SHA256"
+    sha256 "1c182ca5e8625086aad8635f26485601d56ff894d56eac2d62d840cd433a3600"
     url "https://github.com/kadetXx/keymidi/releases/download/v#{version}/KeyMIDI-#{version}-x64.dmg"
   end
 
